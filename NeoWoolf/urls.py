fee4a018a6103892afd7f1d45116494b2e7294a1
Kavbian <kavbian@gmail.com>
2024-11-21 21:45:07 +0200

    Stable version

--- NeoWoolf/urls.py
+++ NeoWoolf/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("shop/", include("Shop.urls", namespace="shop")),
+    path("", include("Shop.urls", namespace="shop")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
